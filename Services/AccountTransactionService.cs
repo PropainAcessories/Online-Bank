67146b2e10d13e977d5c4396e1008053b5ef0003
HankHill <hhc3h8@gmail.com>
2023-04-13 12:25:24 -0500

    Services done. Short break, then make basic UI and polish.

--- Services/AccountTransactionService.cs
+++ Services/AccountTransactionService.cs
@@ -1,7 +1,63 @@
-﻿namespace Online_Bank.Services
+﻿using Online_Bank.Data.Contexts;
+using Online_Bank.Data.DAOs;
+using Online_Bank.Data.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Bank.Services
 {
   public class AccountTransactionService
   {
+    private AccountTransactionDAO accountTransactionDAO;
 
+    public AccountTransactionService(ProjectContext context)
+    {
+      this.accountTransactionDAO = new AccountTransactionDAO(context);
+    }
+
+    public List<TransactionDTO> GetAllTransactionOfAccount(int accountNumber)
+    {
+      return this.accountTransactionDAO.GetTransactionByAccountNumber(accountNumber).ToList();
+    }
+
+    public List<AccountDTO> GetAllAccountOfTransaction(int transactionId)
+    {
+      return this.accountTransactionDAO.GetAccountByTransactionId(transactionId).ToList();
+    }
+
+    public Boolean LinkTransactionToAccount(int  transactionId, int accountNumber)
+    {
+      AccountTransactionDTO createdLink = this.accountTransactionDAO.GetByAccountNumberAndTransactionId(accountNumber, transactionId);
+      return this.accountTransactionDAO.Create(createdLink);
+    }
+
+    public AccountTransactionDTO UnlinkTransactionToAccount(int transactionId, int accountNumber)
+    {
+      AccountTransactionDTO deletedLink = this.accountTransactionDAO.GetByAccountNumberAndTransactionId(accountNumber, transactionId);
+      return this.accountTransactionDAO.Delete(deletedLink);
+    }
+
+    public void UnlinkAllTransactionsFromAccount(int accountNumber)
+    {
+      List<TransactionDTO> transactionList = this.GetAllTransactionOfAccount(accountNumber);
+      foreach (TransactionDTO transaction in transactionList)
+      {
+        AccountTransactionDTO accountTransactionToDelete = this.accountTransactionDAO.GetByAccountNumberAndTransactionId(accountNumber, transaction.Id);
+        this.accountTransactionDAO.Delete(accountTransactionToDelete);
+      }
+    }
+
+    public void UnlinkAllAccountFromTransaction(int transactionId)
+    {
+      List<AccountDTO> accountOfTransaction = this.accountTransactionDAO.GetAccountByTransactionId(transactionId);
+      foreach (AccountDTO account in accountOfTransaction)
+      {
+        AccountTransactionDTO accountTransactionToDelete = this.accountTransactionDAO.GetByAccountNumberAndTransactionId(account.AccountNumber, transactionId);
+        this.accountTransactionDAO.Delete(accountTransactionToDelete);
+      }
+    }
   }
 }
